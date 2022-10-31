28616fed407dffde588f894f4913134884f5fd94
SuhovenkoEduard <73432765+SuhovenkoEduard@users.noreply.github.com>
2022-10-31 16:22:11 +0300

    =android lab3 react-native ts-eslint setup

---
+++ android/lab3_rn/App.styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    borderRadius: 10,
+    borderWidth: 3,
+    borderStyle: "dashed",
+    borderColor: "black",
+    padding: 10,
+    margin: 10
+  }
+})
---
+++ android/lab3_rn/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useState } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { styles } from './App.styles'
+
+const App = () => {
+  const [isChecked, setIsChecked] = useState<boolean>(true)
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{isChecked ? "Checked" : "UnChecked"}</Text>
+      <TouchableOpacity style={styles.button} onPress={() => setIsChecked(!isChecked)}>
+        <Text>Change isChecked flag</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+export default App
